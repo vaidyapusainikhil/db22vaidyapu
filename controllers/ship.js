4a93d5b31aaa2a0ad803beded0bf080f556ea708
vaidyapusainikhil <89408610+vaidyapusainikhil@users.noreply.github.com>
2021-11-12 10:59:31 -0600

    Added details

--- controllers/ship.js
+++ controllers/ship.js
@@ -1,9 +1,16 @@
 var Ship = require('../models/ship'); 
  
 // for a specific ship. 
-exports.ship_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: ship detail: ' + req.params.id); 
-}; 
+exports.ship_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Ship.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+};  
  
 // Handle Ship create on POST. 
 exports.ship_create_post = async function(req, res) { 
@@ -32,8 +39,24 @@ exports.ship_delete = function(req, res) {
 }; 
  
 // Handle ship update form on PUT. 
-exports.ship_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: ship update PUT' + req.params.id); 
+exports.ship_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Ship.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.Company)  
+               toUpdate.Company = req.body.Company; 
+        if(req.body.Cost) toUpdate.Cost = req.body.Cost; 
+        if(req.body.Colour) toUpdate.Colour = req.body.Colour; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
 }; 
 
 // List of all Ship
--- routes/resource.js
+++ routes/resource.js
@@ -13,19 +13,18 @@ router.get('/resource', api_controller.api);
 /// ship ROUTES /// 
  
 // POST request for creating a ship.  
-router.post('/resource/ships', ship_controller.ship_create_post); 
+router.post('/resource/ship', ship_controller.ship_create_post); 
  
 // DELETE request to delete ship. 
-router.delete('/resource/ships/:id', ship_controller.ship_delete); 
+router.delete('/resource/ship/:id', ship_controller.ship_delete); 
  
 // PUT request to update ship. 
-router.put('/resource/ships/:id', 
-ship_controller.ship_update_put); 
+router.put('/resource/ship/:id', ship_controller.ship_update_put); 
  
 // GET request for one ship. 
-router.get('/resource/ships/:id', ship_controller.ship_detail); 
+router.get('/resource/ship/:id', ship_controller.ship_detail); 
  
 // GET request for list of all ship items. 
-router.get('/resource/ships', ship_controller.ship_list); 
+router.get('/resource/ship', ship_controller.ship_list); 
  
 module.exports = router;
