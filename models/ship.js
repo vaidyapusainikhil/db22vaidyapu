7c3c0c36d00589e125f2d85a6ac40ec33f7ad6df
vaidyapusainikhil <89408610+vaidyapusainikhil@users.noreply.github.com>
2021-12-01 22:41:50 -0600

    added requirements

--- models/ship.js
+++ models/ship.js
@@ -1,9 +1,16 @@
-const mongoose = require("mongoose") 
-const shipSchema = mongoose.Schema({ 
-Company : String, 
-Cost : Number, 
-Colour : String 
-}) 
+// const mongoose = require("mongoose") 
+// const shipSchema = mongoose.Schema({ 
+// Company : String, 
+// Cost : {type:Number,min:14000,max:40000},
+// Colour : String 
+// }) 
  
-module.exports = mongoose.model("Ship", 
-shipSchema) 
+// module.exports = mongoose.model("Ship", 
+// shipSchema) 
+const mongoose = require("mongoose")
+const fairydollSchema = mongoose.Schema({
+Company: String,
+cost: {type:Number,min:14000,max:40000},
+Color: String,
+} )
+module.exports = mongoose.model("fairydoll",fairydollSchema)
--- routes/ship.js
+++ routes/ship.js
@@ -1,11 +1,18 @@
 var express = require('express'); 
 const ship_controlers= require('../controllers/ship'); 
 var router = express.Router(); 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET ships */ 
 router.get('/', ship_controlers.ship_view_all_Page ); 
 router.get('/detail', ship_controlers.ship_view_one_Page); 
 router.get('/create', ship_controlers.ship_create_Page); 
-router.get('/update', ship_controlers.ship_update_Page); 
-router.get('/delete', ship_controlers.ship_delete_Page); 
+router.get('/update',secured, ship_controlers.ship_update_Page); 
+router.get('/delete',secured, ship_controlers.ship_delete_Page); 
 module.exports = router;
